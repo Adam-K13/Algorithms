913d27306a2ddf606b2a1fcc194e0c9385fbc8a6
Adam Kearns <adam.kearns1@ucdconnect.ie>
2025-04-18 22:17:21 +0100

    mac results added

--- Main.java
+++ Main.java
@@ -29,7 +29,7 @@ public class Main {
             testArrays.put(size, arrayGenerator.generateRandomArray(size));
         }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("sorting_benchmark.csv"))) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("sorting_benchmark_mac.csv"))) {
             // Write CSV header with both time units
             writer.write("Algorithm,Array Size,Time (ms),Time (s)\n");
 
